c4c697717e217ef76608199dbb19ec743be04b8c
Elijah Moore <e.moore8902@gmail.com>
2022-05-27 20:48:41 -0700

    started the javascript to create the grid

---
+++ sketch.js
@@ -0,0 +1,24 @@
+const grid = document.querySelector('.grid');
+let gridValue = document.querySelector('.grid-size');
+let gridSize = document.querySelector('input');
+const resetBtn = document.querySelector('.reset');
+const applyGridSize = document.querySelector('.apply');
+let squareSize = 8;
+
+createGrid(squareSize);
+
+function createGrid(squareSize){
+    for (let i = 0; i < gridSize; i ++){
+        for (let j = 0; j < gridSize; j ++){
+            grid.appendChild(createDiv(grid.clientWidth/gridSize));
+        }
+    }
+}
+
+function createDiv(size){
+    const Div = document.createElement('div');
+    Div.classList.add('box');
+    Div.style.width = '${size}px';
+    Div.style.height = '${size}8px';
+}
+
