00824b1a68b5707d5cb716461f41e9cdd55db5d3
Elijah Moore <e.moore8902@gmail.com>
2022-05-28 20:36:04 -0700

    added input to change grid size, continued styling, added reset button

--- sketch.js
+++ sketch.js
@@ -4,23 +4,41 @@ const resetButton = document.querySelector(".reset");
 
 createGrid = () => {
     for (let i = 0; i < 256; i ++){
-    const div = document.createElement("div");
-    div.classList.add("square");
-    grid.appendChild(div);
+        const div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
     }
 }
+
 
 updateGrid = () => {
-    for (let i = 0; i < userInput * userInput; i ++) {
+    grid.innerHTML = '';
+    grid.style.setProperty("grid-template-columns", `repeat(${userInput.value}, 2fr)`);
+    console.log(`repeat(${userInput.value}, 2fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(${userInput.value}, 2fr)`);
 
+    for (let i = 0; i < userInput.value*userInput.value; i++) {
+        const div = document.createElement("div");
+        div.classList.add("square");
+        grid.appendChild(div);
     }
 }
 
-const square = document.querySelector("div");
+const square = document.querySelector("div.container");
 square.addEventListener("mouseover", function(event) {
     event.target.classList.replace("square", "color");
+    console.log('hi');
 });
 
+userInput.addEventListener("change", updateGrid);
+
+resetButton.addEventListener("click", function() {
+    grid.innerHTML = "";
+    userInput.value = "";
+    grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+    grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+    createGrid();
+  });
 
 
 createGrid();
